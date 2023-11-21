e71a81851bceddfe30d6dca7c3f987e74e486091
DakshPatel2007 <79900403+DakshPatel2007@users.noreply.github.com>
2023-11-21 18:10:11 +1100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+var nodemailer = require("nodemailer");
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+server.listen(process.env.PORT || 3030);
+
+const transporter = nodemailer.createTransport({
+    port:465,
+    host:"smtp.gmail.com",
+    auth: {
+        user:'',
+        pass:'', 
+    },
+    secure: true,
+});
